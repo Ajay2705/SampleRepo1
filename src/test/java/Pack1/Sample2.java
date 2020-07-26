1390d907f97ec662dadfd4d61d5ac550185cebb0
ajayrajace278@gmail.com <ajaygit@278>
2020-07-26 11:20:48 +0530

    Sample2 added in system

---
+++ src/test/java/Pack1/Sample2.java
@@ -0,0 +1,26 @@
+package Pack1;
+
+import org.testng.annotations.Test;
+
+public class Sample1 {
+
+	@Test
+	public static void m1() {
+		// TODO Auto-generated method stub
+		System.out.println();
+		System.out.println();
+		System.out.println("######## Eclipse Changes #########");
+		System.out.println("======= System changes =======");
+		System.out.println();
+		System.out.println();
+	}
+	public static void m2() {
+		// TODO Auto-generated method stub
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	}
+
+
+}
