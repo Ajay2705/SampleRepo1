bdc9d4e6c2c0434d1d9ba4f25c9fabb5286972fb
ajayrajace278@gmail.com <ajaygit@278>
2020-07-26 21:51:02 +0530

    system second commit

---
+++ src/test/java/Pack1/Sample4.java
@@ -0,0 +1,18 @@
+package Pack1;
+
+import org.testng.annotations.Test;
+
+public class Sample4 {
+
+	@Test
+	public static void m() {
+		// TODO Auto-generated method stub
+		System.out.println();
+		System.out.println();
+		System.out.println("Sample 3 added in eclipse");
+		System.out.println("change for stash");
+		System.out.println("Sample 3 added in eclipse new line in system locally");
+	}
+
+
+}
